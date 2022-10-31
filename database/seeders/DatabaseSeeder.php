d98d7234497054c74aa47cad351f5e42744b1d59
Claudio Conejero <conejero.claudio@gmail.com>
2022-10-31 01:18:44 -0300

    Implemento Link en la navegación

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -24,20 +24,16 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(100)->create();
 
-        $claudio = User::factory()->create([
-            'name' => 'Claudio Conejero',
-            'email' => 'conejero.claudio@gmail.com',
-            'password' => 'password'
-        ]);
-
-        $pepe = User::factory()->create([
-            'name' => 'Pepe Lepew',
-            'email' => 'pepe@lepew.com',
-            'password' => 'password'
-        ]);
-
         $patagonia = Bank::factory()->create([
             'name' => 'Patagonia'
+        ]);
+
+        $frances = Bank::factory()->create([
+            'name' => 'Frances'
+        ]);
+
+        Bank::factory()->create([
+            'name' => 'Nación'
         ]);
 
         $visa = CardBrand::factory()->create([
@@ -46,56 +42,6 @@ class DatabaseSeeder extends Seeder
 
         $master = CardBrand::factory()->create([
             'name' => 'Master'
-        ]);
-
-        Card::factory()->create([
-            'bank_id' => $patagonia,
-            'user_id' => $claudio,
-            'card_brand_id' => $visa,
-            'name' => 'Visa Claudio',
-        ]);
-
-        Card::factory()->create([
-            'bank_id' => $patagonia,
-            'user_id' => $claudio,
-            'card_brand_id' => $master,
-            'name' => 'Master Claudio',
-        ]);
-
-        Card::factory()->create([
-            'bank_id' => $patagonia,
-            'user_id' => $claudio,
-            'card_brand_id' => $visa,
-            'name' => 'Visa Vikky',
-        ]);
-
-        Card::factory()->create([
-            'bank_id' => $patagonia,
-            'user_id' => $claudio,
-            'card_brand_id' => $master,
-            'name' => 'Master Vikky',
-        ]);
-
-        $frances = Bank::factory()->create([
-            'name' => 'Frances'
-        ]);
-
-        Card::factory()->create([
-            'bank_id' => $frances,
-            'user_id' => $claudio,
-            'card_brand_id' => $visa,
-            'name' => 'Visa Frances',
-        ]);
-
-        Card::factory()->create([
-            'bank_id' => $frances,
-            'user_id' => $pepe,
-            'card_brand_id' => $master,
-            'name' => 'Esta no va.',
-        ]);
-
-        Bank::factory()->create([
-            'name' => 'Nación'
         ]);
 
         AccountType::factory()->create([
@@ -114,26 +60,6 @@ class DatabaseSeeder extends Seeder
         $dolar = Currency::factory()->create([
             'name' => 'Dolar',
             'sign' => 'U$D'
-        ]);
-
-        Account::factory()->create([
-            'description' => 'CA Claudio Pesos Patagonia',
-            'alias' => 'RUBIA.EUROPA.NAIPE',
-            'cbu' => '0340230808230016904003',
-            'bank_id' => $patagonia,
-            'account_type_id' => $CA,
-            'currency_id' => $peso,
-            'user_id' => $claudio
-        ]);
-
-        Account::factory()->create([
-            'description' => 'CA Claudio Dolares Patagonia',
-            'alias' => 'TORRE.CLAVE.TE',
-            'cbu' => '0340230809230016904004',
-            'bank_id' => $patagonia,
-            'account_type_id' => $CA,
-            'currency_id' => $dolar,
-            'user_id' => $claudio
         ]);
 
         Movement::factory()->create([
