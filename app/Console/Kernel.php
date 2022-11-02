22af26da8fcd469df8cc702d354efa1a1d0d3300
Claudio Conejero <conejero.claudio@gmail.com>
2022-11-02 19:04:30 -0300

    Cambio del horario del schedule del dolar. (del)

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -35,7 +35,7 @@ class Kernel extends ConsoleKernel
             ]);
         })
             ->timezone('America/Argentina/Ushuaia')
-            ->between('6:00', '6:19')
+            ->between('19:00', '19:19')
             ->name('Valor del dolar');
 
     }
